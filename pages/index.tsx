85cea88626c3a382975da4c5898b142fdc5d890d
Paulo Moutinho <paulo_vicali@icloud.com>
2022-10-05 10:41:44 -0300

    uso do dark/light mode icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { Button, Flex, Heading, Input, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import { Icon } from '@chakra-ui/react'
+import { MdOutlineLightMode, MdModeNight } from "react-icons/md"
 
 const Home: NextPage = () => {
   const { toggleColorMode } = useColorMode()
@@ -12,7 +14,9 @@ const Home: NextPage = () => {
         <Input placeholder='@moutinhofuturedev' textAlign="center" variant="filled" mb={3} type="email"/>
         <Input placeholder='********' textAlign="center" variant="filled" mb={3} type="password"/>
         <Button mb={6} colorScheme='teal'>Log in</Button>
-        <Button onClick={toggleColorMode}>Toggle Color Mode</Button> {/* dark:light mode */}
+        <Button onClick={toggleColorMode}>Toggle Color Mode
+          {formBackground === "gray.700" ? <Icon as={MdOutlineLightMode} ml="3"/> : <Icon as={MdModeNight} ml="3"/>} {/* dark:light mode */}
+        </Button> 
       </Flex>
     </Flex>
   )
